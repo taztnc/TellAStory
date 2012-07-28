e501b8b6c542641b055c352014a9b416d1557774
Yossi Mozgerashvily <yossi1981@gmail.com>
2012-07-27 22:27:07 -0700

    [Core Services Host] Changed the Storage Service Host to be a general host of all core services.

--- Src/TellAStory/StorageServiceHost/Program.cs
+++ Src/TellAStory/StorageServiceHost/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.ServiceModel;
 using StorageService;
+using FacebookAPIService;
 
 namespace Hosts
 {
@@ -11,19 +12,46 @@ namespace Hosts
     {
         static void Main(string[] args)
         {
+            ServiceHost storageServiceHost = null;
+            ServiceHost facebookAPIServiceHost = null;
             try
             {
-                var serviceHost = new ServiceHost(typeof(StupidStorageService));
-                serviceHost.Open();
-                Console.WriteLine("StupidStorageService");
-                Console.ReadKey();
-                serviceHost.Close();
+                storageServiceHost = StartStorageService();
+                facebookAPIServiceHost = StartFacebookAPIService();
+                Console.WriteLine();
+                Console.WriteLine("Press 'F1' to exit");
+                while (Console.ReadKey().Key != ConsoleKey.F1) ;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(string.Format("Exception: {0}", ex.Message));
-                throw;
             }
+
+            if (storageServiceHost != null)
+            {
+                storageServiceHost.Close();
+            }
+
+            if (facebookAPIServiceHost != null)
+            {
+                facebookAPIServiceHost.Close();
+            }
+       }
+
+        private static ServiceHost StartFacebookAPIService()
+        {
+            var facebookAPIServiceHost = new ServiceHost(typeof(FacebookAPIService.FacebookAPIService));
+            facebookAPIServiceHost.Open();
+            Console.WriteLine("Facebook API Service started");
+            return facebookAPIServiceHost;
+        }
+
+        private static ServiceHost StartStorageService()
+        {
+            var storageServiceHost = new ServiceHost(typeof(StupidStorageService));
+            storageServiceHost.Open();
+            Console.WriteLine("Storage Service started");
+            return storageServiceHost;
         }
     }
 }
